5f4941528da165b9e76b43e1443414c2cefcda7f
chrstntdd <christiantodd27@icloud.com>
2017-11-25 22:24:37 -0500

    async'd with an explicit error catch for child_process.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
 import { File, WorkFlowContext, Plugin } from "fuse-box";
-import * as path from "path";
-import * as fs from "fs";
+import { resolve } from "path";
+import { readFile } from "fs";
 import { spawn } from "child_process";
 import { tmpName } from "tmp";
+import { promisify } from "util";
 
-const tmp = () => new Promise((resolve, reject) => tmpName((err, path) => err ? reject(err) : resolve(path)));
+const asyncReadFile = promisify(readFile);
+
+const tmp = () =>
+  new Promise((resolve, reject) =>
+    tmpName((err, path) => (err ? reject(err) : resolve(path)))
+  );
 
 export interface ElmPluginOptions {
-  warn?: boolean,
-  debug?: boolean
+  warn?: boolean;
+  debug?: boolean;
 }
 
 class ElmPluginClass implements Plugin {
-
   // Match Elm files
   public test: RegExp = /\.elm$/;
 
@@ -26,49 +31,50 @@ class ElmPluginClass implements Plugin {
     context.allowExtension(".elm");
   }
 
-  public transform(file: File) {
+  public async transform(file: File) {
     file.loadContents();
 
     // Get the path to elm-make
     const elmMakePath = getElmMakePath();
 
-    return tmp()
-      .then(tmpFilename => tmpFilename + ".js")
-      .then(tmpFilename => new Promise((resolve, reject) => {
-        // Construct the arguments for elm-make
-        const args = [
-          "--yes",
-          "--output",
-          tmpFilename,
-          this.options.warn ? "--warn" : null,
-          this.options.debug ? "--debug" : null,
-          file.absPath
-        ].filter(x => x !== null);
+    const tmpFilename = `${await tmp()}.js`;
 
-        const proc = spawn(elmMakePath, args, { stdio: "inherit" });
+    return await new Promise((resolve, reject) => {
+      // Construct the arguments for elm-make
+      const args = [
+        "--yes",
+        "--output",
+        tmpFilename,
+        this.options.warn ? "--warn" : null,
+        this.options.debug ? "--debug" : null,
+        file.absPath
+      ].filter(x => x !== null);
 
-        proc.on("close", code => {
-          if (code === 0) {
-            fs.readFile(tmpFilename, (err, data) => {
-              if (err) reject(err);
-              file.contents = data.toString();
-              resolve(file);
-            });
-          } else {
-            reject("Failed to compile Elm");
-          }
-        });
-      }));
+      const proc = spawn(elmMakePath, args, { stdio: "inherit" });
+
+      proc.on("error", err => reject(err));
+      proc.on("close", code => {
+        if (code === 0) {
+          readFile(tmpFilename, (err, data) => {
+            err && reject(err);
+            file.contents = data.toString();
+            resolve(file);
+          });
+        } else {
+          reject("Failed to compile Elm.");
+        }
+      });
+    });
   }
-
 }
 
 function getElmMakePath() {
   try {
-    return path.resolve("node_modules/.bin/elm-make");
-  } catch (_) { }
+    return resolve("node_modules/.bin/elm-make");
+  } catch (_) {}
 
   return "elm-make";
 }
 
-export const ElmPlugin = (options?: ElmPluginOptions) => new ElmPluginClass(options);
+export const ElmPlugin = (options?: ElmPluginOptions) =>
+  new ElmPluginClass(options);
